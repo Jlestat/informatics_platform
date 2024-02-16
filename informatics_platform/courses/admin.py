f04c378ccba177e795e8e9282c047b33655564f8
Jlestat <jlestatt@gmail.com>
2024-02-16 22:32:01 +0300

    register models

--- informatics_platform/courses/admin.py
+++ informatics_platform/courses/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Question, Quiz, Course, Lesson, Answer
 
-# Register your models here.
+
+admin.site.register(Question)
+admin.site.register(Quiz)
+admin.site.register(Course)
+admin.site.register(Lesson)
+admin.site.register(Answer)
